a7cb54e50090a0a3a6257493d6d4fcff09e50db2
thetimstarcev <132023910+thetimstarcev@users.noreply.github.com>
2026-05-08 10:55:11 -0400

    Add Dealership and Vehicle classes and all required methods from Phase 1

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,70 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private final ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        inventory = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByPrice(double min, double max) {
+        return null;
+    }
+    public ArrayList<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        return null;
+    }
+    public ArrayList<Vehicle> getVehiclesByYear(int min, int max) {
+        return null;
+    }
+    public ArrayList<Vehicle> getVehiclesByColor(String color) {
+        return null;
+    }
+    public ArrayList<Vehicle> getVehiclesByMileage(int min, int max) {
+        return null;
+    }
+    public ArrayList<Vehicle> getVehiclesByType(String type) {
+        return null;
+    }
+    public ArrayList<Vehicle> getAllVehicles() {
+        return inventory;
+    }
+
+    public void addVehicle(Vehicle vehicle){
+
+
+    }
+
+
 }
